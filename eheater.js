a63f5a75adf09d39cea4487a1b078555f9b717fb
ajinkyac07 <ajinkyachivare15@gmail.com>
2026-03-11 11:05:50 +0530

    Refactor e_heater properties and variable declarations

---
+++ eheater.js
@@ -0,0 +1,54 @@
+runAfterLoad(function() {
+
+elements.e_heater = {
+    color: "#ff8c00",
+    behavior: behaviors.WALL,
+    category: "machines",
+    conduct: 1,
+    insulate: true,
+    hardness: 1,
+    tempGoal: 200,
+
+    desc: "Electric heater with adjustable temperature",
+
+    onSelect: function() {
+        let t = prompt("Set heater temperature:", elements.e_heater.tempGoal);
+        if (t !== null) {
+            elements.e_heater.tempGoal = parseFloat(t);
+        }
+    },
+
+    tick: function(pixel) {
+
+        if (!pixel.charge) return;
+
+        let goal = elements.e_heater.tempGoal;
+
+        let dirs = [
+            [0,-1],[0,1],[-1,0],[1,0],
+            [-1,-1],[1,-1],[-1,1],[1,1]
+        ];
+
+        for (let i = 0; i < dirs.length; i++) {
+
+            let x = pixel.x + dirs[i][0];
+            let y = pixel.y + dirs[i][1];
+
+            if (!isEmpty(x,y,true)) {
+
+                let other = pixelMap[x][y];
+
+                if (other.temp < goal) {
+                    other.temp += 5;
+                    pixelTempCheck(other);
+                }
+
+            }
+
+        }
+
+    }
+
+};
+
+});
